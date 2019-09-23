14758d2ee8417c5982d3e74b6f565d12a7baf101
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2019-09-22 23:01:49 -0700

    Support print! and println! macros in VGA

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(format_args_nl)]
 #![feature(panic_info_message)]
 
 #![no_std]
@@ -7,32 +8,21 @@ use core::panic::PanicInfo;
 mod gdt;
 mod vga;
 
-use vga::*;
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    let mut vga = VGA::new(80, 25);
-    vga.set_cursor(0, 0);
-    core::fmt::write(&mut vga, format_args!("Kernel panic: "));
+    print!("Kernel panic: ");
     if let Some(message) = info.message() {
-        core::fmt::write(&mut vga, *message);
+        vga::_print(*message);
+        println!();
     }
     loop { }
 }
 
 #[no_mangle]
 pub extern "C" fn _entry() -> ! {
-    let mut vga = VGA::new(80, 25);
-    for y in 0..vga.height() {
-        for x in 0..vga.width() {
-            vga.putc(x, y, ' ');
-        }
-    }
-    // TODO: make panics reuse the cursor
-    vga.set_cursor(0, 1);
-    core::fmt::write(&mut vga, format_args!("Booting Myria"));
+    vga::clear_screen();
+    println!("Booting Myria");
     gdt::load_gdt();
-    vga.set_cursor(0, 2);
-    core::fmt::write(&mut vga, format_args!("Loaded GDT"));
+    println!("Loaded GDT");
     panic!("Halt");
 }
--- src/vga.rs
+++ src/vga.rs
@@ -1,14 +1,47 @@
-use core::fmt::{Result, Write};
+use core::fmt::{self, Arguments, Result, Write};
 
-pub struct VGA {
+// Copied from Rust sources
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
+}
+
+// Copied from Rust sources
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ({
+        $crate::vga::_print(format_args_nl!($($arg)*));
+    })
+}
+
+pub fn _print(args: Arguments) {
+    unsafe {
+        fmt::write(&mut __VGA, args).unwrap();
+    }
+}
+
+pub fn clear_screen() {
+    unsafe {
+        for y in 0..__VGA.height() {
+            for x in 0..__VGA.width() {
+                __VGA.putc(x, y, ' ');
+            }
+        }
+    }
+}
+
+struct VGA {
     width: u8,
     height: u8,
     cur_x: u8,
     cur_y: u8,
 }
 
+static mut __VGA: VGA = VGA::new(80, 25);
+
 impl VGA {
-    pub fn new(width: u8, height: u8) -> VGA {
+    pub const fn new(width: u8, height: u8) -> VGA {
         VGA {
             width,
             height,
@@ -17,16 +50,16 @@ impl VGA {
         }
     }
 
-    pub fn width(&self) -> u8 { self.width }
+    fn width(&self) -> u8 { self.width }
 
-    pub fn height(&self) -> u8 { self.height }
+    fn height(&self) -> u8 { self.height }
 
-    pub fn set_cursor(&mut self, x: u8, y: u8) {
+    fn set_cursor(&mut self, x: u8, y: u8) {
         self.cur_x = x;
         self.cur_y = y;
     }
 
-    pub fn putc(&self, x: u8, y: u8, ch: char) {
+    fn putc(&self, x: u8, y: u8, ch: char) {
         if x >= self.width || y >= self.height {
             panic!("Invalid VGA::putc()");
         }
@@ -37,7 +70,7 @@ impl VGA {
         }
     }
 
-    pub fn puts(&self, x: u8, y: u8, s: &str) {
+    fn puts(&self, x: u8, y: u8, s: &str) {
         if s.len() > (self.width - x) as usize || y >= self.height {
             panic!("Invalid VGA::puts()")
         }
@@ -54,8 +87,23 @@ impl VGA {
 
 impl Write for VGA {
     fn write_str(&mut self, s: &str) -> Result {
-        self.puts(self.cur_x, self.cur_y, s);
-        self.cur_x += s.bytes().len() as u8;
+        let mut line = s;
+        while line.len() > 0 {
+            match line.find(|c| c == '\n') {
+                Some(idx) => {
+                    let (part, rest) = line.split_at(idx);
+                    self.puts(self.cur_x, self.cur_y, part);
+                    self.cur_x = 0;
+                    self.cur_y += 1;
+                    line = &rest[1..];
+                }
+                None => {
+                    self.puts(self.cur_x, self.cur_y, line);
+                    self.cur_x += s.bytes().len() as u8;
+                    break;
+                }
+            }
+        }
         Ok(())
     }
 }
